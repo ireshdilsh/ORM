661e3d36aa16b6ce4f2614dcc863d8d5754d18d1
ireshdilsh <ireshdilshan46@gmail.com>
2025-04-11 23:42:57 +0530

    Add DaoFactory class with singleton pattern and daoType enum for ADMIN

--- src/main/java/com/example/orm/dao/DaoFactory.java
+++ src/main/java/com/example/orm/dao/DaoFactory.java
@@ -1,5 +1,25 @@
 package com.example.orm.dao;
 
 public class DaoFactory {
+    private static DaoFactory daoFactory;
 
+    private DaoFactory(){}
+
+    public static DaoFactory getDaoFactory(){
+        return daoFactory != null ? daoFactory : new DaoFactory();
+    }
+
+    public SuperDao getDao(daoType type){
+        switch (type) {
+            case ADMIN:
+                return null;
+        
+            default:
+                return null;
+        }
+    }
+
+    public enum daoType{
+        ADMIN
+    }
 }
