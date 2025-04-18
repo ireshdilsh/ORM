b1bebc6b46202ee250fdd5e60917460e93fad9b5
ireshdilsh <ireshdilshan46@gmail.com>
2025-04-18 15:53:50 +0530

    feat: implement navigation in LandingController and update Landing.fxml layout; add RoleView.fxml

--- src/main/java/com/example/orm/controller/LandingController.java
+++ src/main/java/com/example/orm/controller/LandingController.java
@@ -1,4 +1,23 @@
 package com.example.orm.controller;
+
+import com.example.orm.utils.WindowUtils;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class LandingController {
+    @FXML
+    private AnchorPane LandingPane;
+
+    @FXML
+    void gotoSigInView(ActionEvent event) throws Exception{
+        new WindowUtils().navigateTo("/view/LandingView", LandingPane);
+    }
+
+    @FXML
+    void gotoSignUpView(ActionEvent event) throws Exception{
+        new WindowUtils().navigateTo("/view/SignUpView", LandingPane);
+    }
 
 }
