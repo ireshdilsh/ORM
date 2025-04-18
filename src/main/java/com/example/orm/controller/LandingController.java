0163b1153f0619369a8ef205f4db3fc804f22ec1
ireshdilsh <ireshdilshan46@gmail.com>
2025-04-18 19:28:28 +0530

    refactor: clean up whitespace in LandingController.java

--- src/main/java/com/example/orm/controller/LandingController.java
+++ src/main/java/com/example/orm/controller/LandingController.java
@@ -7,14 +7,13 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 
 public class LandingController {
+
     @FXML
     private AnchorPane LandingPane;
-
     @FXML
     void gotoSigInView(ActionEvent event) throws Exception{
         new WindowUtils().navigateTo("/view/LandingView", LandingPane);
     }
-
     @FXML
     void gotoSignUpView(ActionEvent event) throws Exception{
         new WindowUtils().navigateTo("/view/SignUpView", LandingPane);
