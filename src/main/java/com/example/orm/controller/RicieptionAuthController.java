8baca8ce7883e98614d5f61566cdea9a4c022b9b
ireshdilsh <ireshdilshan46@gmail.com>
2025-04-19 11:46:31 +0530

    update

--- src/main/java/com/example/orm/controller/RicieptionAuthController.java
+++ src/main/java/com/example/orm/controller/RicieptionAuthController.java
@@ -8,8 +8,6 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-import java.io.IOException;
-
 public class RicieptionAuthController {
 
     @FXML
